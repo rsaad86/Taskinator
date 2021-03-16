366dbd686663ea5eda9d4e79b01d36d9a0e3b67d
Rani Saad <rsaad@live.ca>
2021-03-16 01:02:28 -0400

    Save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,7 @@ var createTaskEl = function (taskDataObj) {
   taskDataObj.id = taskIdCounter;
 
   tasks.push(taskDataObj);
+  saveTasks();
   // increase task counter for next unique id
   taskIdCounter++;
 };
@@ -123,6 +124,7 @@ var completeEditTask = function (taskName, taskType, taskId) {
     }
   }
 
+  saveTasks();
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -174,6 +176,8 @@ var taskStatusChangeHandler = function (event) {
     }
   }
   console.log(tasks);
+
+  saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -221,6 +225,11 @@ var deleteTask = function (taskId) {
 
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
+};
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 // Create a new task
